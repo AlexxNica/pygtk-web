2be0bd52e040821bb39695417fd176a0819429b3
Rafael Villar Burke <pachi@mmn-arquitectos.com>
2005-12-05 13:51:04 +0000

    articles/cairo-pygtk-widgets/
    2005-12-05  Rafael Villar Burke  <pachi@mmn-arquitectos.com>
    
            * articles/cairo-pygtk-widgets/
            * articles/cairo-pygtk-widgets/cairo-pygtk-widgets.htm
            * articles/cairo-pygtk-widgets/cairo-pygtk-widgets.css
            * articles/cairo-pygtk-widgets/clock_ex1.png
            * articles/cairo-pygtk-widgets/clock_ex2.png
            * articles/cairo-pygtk-widgets/clock_ex3.png
            * articles/cairo-pygtk-widgets/clock_ex1.py
            * articles/cairo-pygtk-widgets/clock_ex2.py
            * articles/cairo-pygtk-widgets/clock_ex3.py
            * articles/cairo-pygtk-widgets/*: Added previous files
            to hold a local copy of the article.
            * articles.src: fix typo

---
+++ articles/cairo-pygtk-widgets/clock_ex1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# clock_ex1.py
+
+# a pygtk widget that implements a clock face
+# porting of Davyd Madeley's 
+# http://www.gnome.org/~davyd/gnome-journal-cairo-article/clock-ex1.c
+
+# author: Lawrence Oluyede <l.oluyede@gmail.com>
+# date: 03 December 2005
+
+import gtk
+
+class EggClockFace(gtk.DrawingArea):
+    def __init__(self):
+        gtk.DrawingArea.__init__(self)
+        self.connect("expose_event", self.expose)
+        
+    def expose(self, widget, event):
+        return False
+
+def main():
+    window = gtk.Window()
+    clock = EggClockFace()
+    
+    window.add(clock)
+    window.connect("destroy", gtk.main_quit)
+    window.show_all()
+    
+    gtk.main()
+    
+if __name__ == "__main__":
+    main()
